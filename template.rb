a3303848b0b7d4408695ff31256d86557875e46d
hisonl <hisonlex@gmail.com>
2015-02-08 10:48:07 +0900

    add end

--- template.rb
+++ template.rb
@@ -198,15 +198,16 @@ gsub_file 'config/database.yml', /ENVNAME/, @env_name
 # MySQL settings
 run "touch config/initializers/innodb_row_format.rb"
 insert_into_file 'config/initializers/innodb_row_format.rb', %(ActiveSupport.on_load :active_record do
-module ActiveRecord::ConnectionAdapters
-  module SchemaStatements
-    def create_table_with_innodb_row_format(table_name, options = {})
-      table_options = options.merge(options: "ENGINE=InnoDB ROW_FORMAT=COMPRESSED")
-      create_table_without_innodb_row_format(table_name, table_options) do |td|
-        yield td if block_given?
+  module ActiveRecord::ConnectionAdapters
+    module SchemaStatements
+      def create_table_with_innodb_row_format(table_name, options = {})
+        table_options = options.merge(options: "ENGINE=InnoDB ROW_FORMAT=COMPRESSED")
+        create_table_without_innodb_row_format(table_name, table_options) do |td|
+          yield td if block_given?
+        end
       end
+      alias_method_chain :create_table, :innodb_row_format
     end
-    alias_method_chain :create_table, :innodb_row_format
   end
 end
 ), before: ''
